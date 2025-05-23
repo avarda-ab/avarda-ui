b60d33b8c39bdb35e0e782b18921b038480a01ee
Marek Červinka <marek.cervinka@tfbank.se>
2025-05-24 01:04:05 +0200

    redid controls decoders

--- src/Stories/Button.elm
+++ src/Stories/Button.elm
@@ -2,26 +2,38 @@ module Stories.Button exposing (main)
 
 import Browser
 import Html exposing (Html)
+import Json.Decode as Decode
 import Ui.Button
-import Util.ControlsHelper exposing (ControlsFlags, ControlsModel, LabelControls, decodeControls)
+import Util.ControlsHelper exposing (ControlsFlags, ControlsModel, decodeControls, decodeStringControl)
 
 
-type Msg
-    = UserClickedButton
+type alias Controls =
+    { label : String }
 
 
-view : LabelControls -> Html Msg
+view : Controls -> Html msg
 view { label } =
     Ui.Button.view label
 
 
-main : Program ControlsFlags (ControlsModel LabelControls) Msg
+main : Program ControlsFlags (ControlsModel Controls) msg
 main =
     Browser.element
         { init =
             \flags ->
-                ( { controls = decodeControls flags { field = "label", fallback = "Click me" } LabelControls }, Cmd.none )
+                ( { controls = decodeControls flags decoder defaultControls }, Cmd.none )
         , update = \_ model -> ( model, Cmd.none )
         , subscriptions = \_ -> Sub.none
         , view = \{ controls } -> view { label = controls.label }
         }
+
+
+decoder : Decode.Decoder Controls
+decoder =
+    Decode.succeed Controls
+        |> decodeStringControl { field = "label", fallback = defaultControls.label }
+
+
+defaultControls : Controls
+defaultControls =
+    { label = "Click me" }
--- src/Util/ControlsHelper.elm
+++ src/Util/ControlsHelper.elm
@@ -1,32 +1,37 @@
-module Util.ControlsHelper exposing (ControlsFlags, ControlsModel, LabelControls, decodeControls)
+module Util.ControlsHelper exposing (ControlsFlags, ControlsModel, decodeBoolControl, decodeControls, decodeStringControl)
 
 import Json.Decode as Decode
-
-
-type alias ControlsPayload =
-    { field : String, fallback : String }
+import Json.Decode.Pipeline as Pipeline
 
 
 type alias ControlsFlags =
     Decode.Value
 
 
-type alias LabelControls =
-    { label : String }
+decodeStringControl : StringControlsPayload -> Decode.Decoder (String -> b) -> Decode.Decoder b
+decodeStringControl { field, fallback } =
+    Pipeline.optional field Decode.string fallback
+
+
+decodeBoolControl : BoolControlsPayload -> Decode.Decoder (Bool -> b) -> Decode.Decoder b
+decodeBoolControl { field, fallback } =
+    Pipeline.optional field Decode.bool fallback
+
+
+type alias StringControlsPayload =
+    { field : String, fallback : String }
+
+
+type alias BoolControlsPayload =
+    { field : String, fallback : Bool }
 
 
 type alias ControlsModel controls =
     { controls : controls }
 
 
-decodeControlWithFallback : ControlsPayload -> (String -> a) -> Decode.Decoder a
-decodeControlWithFallback { field, fallback } controls =
-    Decode.maybe (Decode.field field Decode.string)
-        |> Decode.map (Maybe.withDefault fallback >> controls)
-
-
-decodeControls : ControlsFlags -> ControlsPayload -> (String -> a) -> a
-decodeControls flags ({ fallback } as controlsPayload) controls =
+decodeControls : ControlsFlags -> Decode.Decoder a -> a -> a
+decodeControls flags decoder defaultControls =
     flags
-        |> Decode.decodeValue (decodeControlWithFallback controlsPayload controls)
-        |> Result.withDefault (controls fallback)
+        |> Decode.decodeValue decoder
+        |> Result.withDefault defaultControls
