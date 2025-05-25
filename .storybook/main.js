2706f2a79563f75770143d21959ca8d1e0fe5ad2
Marek Červinka <marek.cervinka@tfbank.se>
2025-05-25 16:53:52 +0200

    added a11y addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,11 @@ import { plugin } from "vite-plugin-elm";
 /** @type { import('@storybook/html-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
+  ],
   framework: {
     name: "@storybook/html-vite",
     options: {},
